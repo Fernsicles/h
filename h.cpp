bc2126185721b7fc7628eef5e3faa49a04da6fb0
Ferns <jeffreyzhang123@gmail.com>
2021-05-29 16:38:09 -0700

    Use getopt and add additional characters

--- h.cpp
+++ h.cpp
@@ -1,35 +1,113 @@
 #include <string>
 #include <iostream>
 #include <cctype>
+#include <unistd.h>
 
 using namespace std;
 int main(int argc, char **argv) {
-	string input = string(argv[2]);
-	string output = "";
-	if(string("e").compare(argv[1]) == 0) {
-		for(int i = 0; input[i] != 0; i++) {
-			unsigned char x = toupper(input[i]);
-			x -= 64;
-			for(int j = 0b1; j <= 0b10000; j <<= 1) {
-				if(x & j) {
-					output.push_back('H');
-				} else {
-					output.push_back('h');
-				}
-			}
+	extern char *optarg;
+	extern int optind;
+	int c, err = 0;
+	int eflag = 0, dflag = 0, hflag = 0;
+	char *estring, *dstring;
+	static char usage[] = "usage: h [-h] [-d decode_string] [-e encode_string]";
+	while((c = getopt(argc, argv, "hd:e:")) != -1) {
+		switch(c) {
+			case 'h':
+				hflag = 1;
+				break;
+			case 'd':
+				dflag = 1;
+				dstring = optarg;
+				break;
+			case 'e':
+				eflag = 1;
+				estring = optarg;
+				break;
+			default:
+				cout << endl;
+				abort();
 		}
 	}
-	if(string("d").compare(argv[1]) == 0) {
-		for(int i = 0; input[i] != 0; i += 5) {
-			unsigned char x = 0b0;
-			for(int j = 0; j < 5; j++) {
-				if(input[i + j] == 'H') {
-					x |= (0b1 << j);
+
+	if(hflag) {
+		cout << usage << endl;
+	} if(dflag) {
+		decode(dstring);
+	} if(eflag) {
+		encode(estring);
+	}
+}
+
+void encode(char *encode_string) {
+	for(int i = 0; encode_string[i] != 0; i++) {
+		unsigned char x = toupper(encode_string[i]);
+		switch(x) {
+			case '!':
+				cout << "hHhHH";
+				break;
+			case '"':
+				cout << "HHhHH";
+				break;
+			case '\'':
+				cout << "hhHHH";
+				break;
+			case '.':
+				cout << "HhHHH";
+				break;
+			case ',':
+				cout << "hHHHH";
+				break;
+			case '?':
+				cout << "HHHHH";
+				break;
+			default:
+				x -= 65;
+				for(int j = 0b1; j <= 0b10000; j <<= 1) {
+					if(x & j) {
+						cout << "H";
+					} else {
+						cout << "h";
+					}
 				}
-			}
-			x += 64;
-			output.push_back(x);
+				break;
 		}
 	}
-	cout << output << endl;
+	cout << endl;
+}
+
+void decode(char *decode_string) {
+	for(int i = 0; decode_string[i] != 0; i += 5) {
+		unsigned char x = 0b0;
+		for(int j = 0; j < 5; j++) {
+			if(decode_string[i + j] == 'H') {
+				x |= (0b1 << j);
+			}
+		}
+		switch(x) {
+			case 26:
+				x = '!';
+				break;
+			case 27:
+				x = '"';
+				break;
+			case 28:
+				x = '\'';
+				break;
+			case 29:
+				x = '.';
+				break;
+			case 30:
+				x = ',';
+				break;
+			case 31:
+				x = '?';
+				break;
+			default:
+				x += 65;
+				break;
+		}
+		cout << x;
+	}
+	cout << endl;
 }
